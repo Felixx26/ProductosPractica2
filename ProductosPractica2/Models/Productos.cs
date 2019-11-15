f387785a1016e20dab6db63428558942b72bf752
Felixx26 <felixxrafaell@gmail.com>
2019-11-14 21:30:34 -0400

    Commit

---
+++ ProductosPractica2/Models/Productos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductosPractica2.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string Categoria { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string Codigo { get; set; }
+        [Required]
+        public int Existencia { get; set; }
+        [Required]
+        public DateTime FechaFab { get; set; }
+        [Required]
+        public DateTime FechaVen { get; set; }
+        [Required]
+        public Boolean Estado { get; set; }
+    }
+}
